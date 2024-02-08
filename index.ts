87601d6998c423e37cf877bf6b7bd76524870596
arnnvv <arnav.900@protonmail.com>
2024-02-08 17:15:04 +0530

    relations

--- index.ts
+++ index.ts
@@ -24,6 +24,27 @@ CREATE TABLE users (
   }
 })();
 
+(async () => {
+  try {
+    await client.connect();
+    await client.query(`CREATE TABLE addresses (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    country VARCHAR(100) NOT NULL,
+    street VARCHAR(255) NOT NULL,
+    pincode VARCHAR(20),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);`);
+  } catch (e) {
+    console.error(`Error in Creating Address Table: ${e}`);
+    throw e;
+  } finally {
+    await client.end();
+  }
+})();
+
 const insertUser = async (
   username: string,
   email: string,
